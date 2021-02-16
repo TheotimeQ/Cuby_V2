ddc4cae1194625ce94131d47d8679a3e2798a439
Zelinsta <53791307+TheotimeQ@users.noreply.github.com>
2021-02-16 16:33:27 +0100

    Clear

--- Cuby_V2_mybro/Init.ino
+++
@@ -1,9 +0,0 @@
-void InitLed(){
-
-    //Setup LED
-    FastLED.addLeds<WS2812B, Pin_Strip_1>(Strip_1, NUM_LEDS_STRIP_1); //Strip 1
-    FastLED.addLeds<WS2812B, Pin_Strip_2>(Strip_2, NUM_LEDS_STRIP_2); //Strip 2
-    LED_POWER(Next_LED_Brightness) ;
-
-  
-}
